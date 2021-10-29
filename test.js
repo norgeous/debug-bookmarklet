ce6f7cbd736c12d79deecee2ef82b4f7b017562b
im-dharte <daniel@harte.me>
2021-10-29 16:25:05 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,29 +1,32 @@
 (() => {
-
-const getHeaders = async () => {
-  const res = await fetch(window.location.href);
-  const h = {};
-  res.headers.forEach((v,k) => h[k] = v);
-  return h;
-};
-
-const purge = async () => {
-  await fetch(window.location, { method:'PURGE' });
-  window.location.reload();
-};
-
 document.body.insertAdjacentHTML(
 'afterbegin',
 `
 <style>
-body {background:red;}
-#debug-tool-panel {position:fixed; top:50%; left:50%}
+#debug-tool-panel {position:fixed; top:50%; left:50%; transform:translate(-50%, -50%); z-index:1000; padding:50px; background:grey; color:white;}
 </style>
 
-<div id="debug-tool-panel">
-PANEL
-</div>
+<pre id="debug-tool-panel">
+
+<button onclick="
+const purge = async () => {
+  const res = await fetch(window.location, { method:'PURGE' });
+  window.location.reload();
+};
+purge();
+">PURGE</button>
+
+<button onclick="
+const getHeaders = async () => {
+  const res = await fetch(window.location.href);
+  const h = {};
+  res.headers.forEach((v,k) => h[k] = v);
+  document.querySelector('#debug-tool-panel').innerHTML = Object.entries(h).map(([k,v]) => k+': '+v).join('\\n');
+};
+getHeaders();
+">HEADERS</button>
+
+</pre>
 `,
 );
-
 })();
