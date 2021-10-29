10725d72f102f301c62f8f275b76cd8134da329d
im-dharte <daniel@harte.me>
2021-10-29 15:50:18 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,12 @@
-fetch(window.location.href).then((res) => {
+const getHeaders = fetch(window.location.href).then((res) => {
   const h = {};
   res.headers.forEach((v,k) => h[k] = v);
-  console.table(h);
+  return h;
 });
+
+const purge = async () => {
+  await fetch(window.location, { method:'PURGE' });
+  window.location.reload();
+};
+
+document.body.insertAdjacentHTML('afterstart', '<div id="two">two</div>');
