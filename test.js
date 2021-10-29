3ba5a213c917be1075b1f2b4761f21ba1165be29
im-dharte <daniel@harte.me>
2021-10-29 15:59:39 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,14 +1,14 @@
 (() => {
-  // const getHeadersXXX = fetch(window.location.href).then((res) => {
-  //   const h = {};
-  //   res.headers.forEach((v,k) => h[k] = v);
-  //   return h;
-  // });
+  const getHeaders = fetch(window.location.href).then((res) => {
+    const h = {};
+    res.headers.forEach((v,k) => h[k] = v);
+    return h;
+  });
 
-  // const purge = async () => {
-  //   await fetch(window.location, { method:'PURGE' });
-  //   window.location.reload();
-  // };
+  const purge = async () => {
+    await fetch(window.location, { method:'PURGE' });
+    window.location.reload();
+  };
 
   document.body.insertAdjacentHTML('afterbegin', '<div id="two">two</div>');
 })();
